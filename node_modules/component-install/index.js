badd568c7008cf0f47dbfd47f02df25bf2aa58cc
Jess <jessicaes2000@gmail.com>
2020-06-08 01:20:10 -0500

    [ADD] Added navbar

---
+++ node_modules/component-install/index.js
@@ -0,0 +1,68 @@
+var remote = 'https://raw.github.com';
+
+var Q = require('q');
+var join = require('path').join;
+var write = Q.nfbind(require('fs').writeFile);
+var read = Q.nfbind(require('fs').readFile);
+var mkdirp = require('mkdirp');
+var download = require('./lib/download-file');
+
+module.exports = install;
+function install(directory, dev, callback) {
+  return read(join(directory, 'component.json'))
+    .then(function (data) {
+      try {
+        data = JSON.parse(data);
+      } catch (ex) {
+        err.message += ' in component.json';
+        throw err;
+      }
+      return installDependencies(data, join(directory, 'components'), dev || false, {}).nodeify(callback);
+    })
+}
+
+function url(name, version, file) {
+  if (version === '*') version = 'master';
+  return remote + '/' + name + '/' + (version || 'master') + '/' + file;
+}
+
+function installDependency(name, version, destination, cache) {
+  return Q.all([download(url(name, version, 'component.json'), {json: true}), mkdirp(join(destination, name.split('/').join('-')))])
+    .spread(function (config) {
+      config.repo = config.repo || remote + '/' + name;
+      return Q.all([
+        installDependencies(config, destination, false, cache),
+        downloadFiles(name, version, config, join(destination,name.split('/').join('-'))),
+        write(join(destination, name.split('/').join('-'), 'component.json'), JSON.stringify(config, null, 2))
+      ]);
+    });
+}
+
+function downloadFiles(name, version, config, destination) {
+  var files = [];
+  if (config.scripts) files = files.concat(config.scripts);
+  if (config.styles) files = files.concat(config.styles);
+  if (config.templates) files = files.concat(config.templates);
+  if (config.files) files = files.concat(config.files);
+  if (config.images) files = files.concat(config.images);
+  if (config.fonts) files = files.concat(config.fonts);
+  return Q.all(files.map(function (file) {
+    return download(url(name, version, file), {destination: join(destination, file)});
+  }))
+}
+function installDependencies(config, destination, dev, cache) {
+  var dependencies = [];
+  if (config.dependencies) dependencies = normalize(config.dependencies);
+  if (dev && config.development) dependencies = dependencies.concat(normalize(config.development));
+  return Q.all(dependencies.map(function (dep) {
+    if (cache['dep: ' + dep]) return;
+    else cache['dep: ' + dep] = true;
+    return installDependency(dep[0], dep[1], destination, cache);
+  }));
+}
+
+function normalize(deps) {
+  return Object.keys(deps).map(function(name){
+    return [name, deps[name]];
+  });
+}
---
+++ node_modules/component-install/test/install.js
@@ -0,0 +1,89 @@
+require('mocha-as-promised')();
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var join = require('path').join;
+var fs = require('fs');
+var assert = require('assert');
+var install = require('../');
+
+describe('install', function () {
+  beforeEach(function (done) {
+    copyDir(join(__dirname, 'fixture', 'install'), join(__dirname, 'output', 'install'), done);
+  });
+  describe('install(directory, false)', function () {
+    it('installs the component along with its dependencies to that directory', function () {
+      return install(join(__dirname, 'output', 'install'), false)
+        .then(function () {
+          equalDir(join(__dirname, 'output', 'install'), join(__dirname, 'fixture', 'install-res-1'));
+        });
+    });
+  });
+  describe('install(directory, true)', function () {
+    it('installs the component along with its dependencies + development dependencies to that directory', function () {
+      return install(join(__dirname, 'output', 'install'), true)
+        .then(function () {
+          equalDir(join(__dirname, 'output', 'install'), join(__dirname, 'fixture', 'install-res-2'));
+        });
+    });
+  });
+  afterEach(function () {
+    rimraf.sync(join(__dirname, 'output'));
+  });
+});
+
+function copyDir(src, dest, cb) {
+  mkdirp(dest, function (err) {
+    if (err) return cb(err);
+    var remaining = 1;
+    var files = fs.readdirSync(src);
+    for(var i = 0; i < files.length; i++) {
+      var current = fs.statSync(join(src, files[i]));
+      if(current.isDirectory()) {
+        remaining++;
+        copyDir(join(src, files[i]), join(dest, files[i]), completeOne);
+      } else if (current.isFile()) {
+        remaining++;
+        copy(join(src, files[i]), join(dest, files[i]), completeOne);
+      }
+    }
+    completeOne();
+    var done = false;
+    function completeOne(err) {
+      if (err && !done) {
+        done = true;
+        return cb(err);
+      }
+      if (0 === --remaining && !done) {
+        done = true;
+        return cb();
+      }
+    }
+  });
+}
+function copy(src, dest, cb) {
+  var oldFile = fs.createReadStream(src);
+  var newFile = fs.createWriteStream(dest);
+  oldFile.pipe(newFile);
+  oldFile.on('error', cb);
+  newFile.on('error', cb);
+  newFile.on('close', cb);
+}
+
+function equalDir(src, dest) {
+  var files = fs.readdirSync(src);
+  for(var i = 0; i < files.length; i++) {
+    var current = fs.statSync(join(src, files[i]));
+    if(current.isDirectory()) {
+      equalDir(join(src, files[i]), join(dest, files[i]));
+    } else if (current.isFile()) {
+      equal(join(src, files[i]), join(dest, files[i]));
+    }
+  }
+}
+function equal(src, dest) {
+  if (/\.js$/i.test(src) || /\.json$/i.test(src) || /\.html$/i.test(src) || /\.css$/i.test(src)) {
+    var a = fs.readFileSync(src).toString().replace(/\r/g, '');
+    var b = fs.readFileSync(dest).toString().replace(/\r/g, '');
+    assert.equal(a, b);
+  }
+}
