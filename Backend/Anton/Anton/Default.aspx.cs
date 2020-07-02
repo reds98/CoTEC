1cdc5a268ab4cdeb6c5197bfaadcfe649ee77d09
Jess <jessicaes2000@gmail.com>
2020-07-02 16:14:42 -0500

    [UPDATE] Migrated Anton to CR project

---
+++ Backend/Anton/Anton/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+
+using CrystalDecisions.Shared;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Web;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
